281f3cf3a1fcb5f90cd768288a0ddafb401728fd
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-04-04 11:20:12 +0300

    Implemented contribute-problem-confirm template

--- src/app/contribute-problem/components/contribute-problem-confirm/contribute-problem-confirm.component.ts
+++ src/app/contribute-problem/components/contribute-problem-confirm/contribute-problem-confirm.component.ts
@@ -1,11 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { LanguageService } from 'src/app/core/base/language.base';
 import { ContributeProblemFormData } from '../contribute-problem-form/contribute-problem-form.component';
 
 @Component({
   selector: 'app-contribute-problem-confirm',
   templateUrl: './contribute-problem-confirm.component.html',
+  styleUrls: ['contribute-problem-confirm.style.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ContributeProblemConfirmComponent {
   @Input() formData!: ContributeProblemFormData;
   @Output() onPrevStep = new EventEmitter<any>();
+  dictionary: any;
+
+  constructor(languageService: LanguageService) {
+    this.dictionary = languageService.dictionary;
+  }
 }
