3ba7d358a1538db59984b589364d4122aa8afae4
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-04-04 23:56:14 +0300

    added problem-contribution api service and model

---
+++ src/app/core/models/problem-contribution.model.ts
@@ -0,0 +1,10 @@
+export interface ProblemContribution {
+    id?: number;
+    title: string;
+    description: string;
+    solution: string;
+    testcase: string;
+    status?: string;
+    statuDetails?: string;
+    createdTime?: string;
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  signInRedirectUri: 'http://localhost:4200/home'
+  apiRoot: 'http://localhost:8080/api'
 };
 
 /*
