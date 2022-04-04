57e1fc09be57128dff3f365fa90304a80c40ba0d
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-04-04 23:55:39 +0300

    Changed sign in redirect URI

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { LanguageService } from 'src/app/core/base/language.base';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-landing',
@@ -16,7 +15,7 @@ export class LandingComponent {
 
   onSignIn() {
     this.keycloakService.login({
-      redirectUri: environment.signInRedirectUri
+      redirectUri: window.location.origin + '/home'
     });
   }
 }
