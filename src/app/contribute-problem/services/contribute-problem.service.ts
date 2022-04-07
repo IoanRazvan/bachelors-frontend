12a9f8d69a6b42739817eb35c02b983ffc8183d4
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-04-07 12:06:09 +0300

    created service for contribute-problem module

---
+++ src/app/contribute-problem/services/contribute-problem.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { ReplaySubject, Subject } from "rxjs";
+import { Page } from "src/app/models/page.model";
+import { ProblemContributionService } from "src/app/core/services/problem-contribution.service";
+import { ProblemContributionResponse } from "src/app/models/problem-contribution.model";
+import { ServerResponse } from "src/app/models/server-response.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ContributeProblemService {
+    private contributions: Subject<ServerResponse<Page<ProblemContributionResponse>>> = new ReplaySubject(1);
+    readonly contributions$ = this.contributions.asObservable();
+    private serverPageSize = 16;
+    private clientPageSize = 8;
+    private currentServerPage!: Page<ProblemContributionResponse>;
+
+
+    constructor(private apiService: ProblemContributionService) {
+    }
+
+    change(page: number, force: boolean = false) {
+        const requestedServerPage = Page.convertPageNumber(page, this.clientPageSize, this.serverPageSize);
+        if (this.currentServerPage == null || requestedServerPage != this.currentServerPage.page || force) {
+            this.apiService.get(requestedServerPage, this.serverPageSize).subscribe({
+                next: (resp) => {
+                    this.currentServerPage = new Page(resp);
+                    this.contributions.next({
+                        response: this.currentServerPage.convertPage(page, this.clientPageSize),
+                        error: false
+                    })
+                },
+                error: () => this.contributions.next({
+                    error: true
+                })
+            });
+        } else {
+            this.contributions.next({
+                response: this.currentServerPage.convertPage(page, this.clientPageSize),
+                error: false
+            });
+        }
+    }
+}
