a6377ee3da2a12a59e9153ec804c1814939e3f47
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-05-11 09:22:57 +0300

    Added assigned-contributions stub component

---
+++ src/app/manage-contributions/components/assigned-contributions/assigned-contributions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assigned-contributions',
+  templateUrl: './assigned-contributions.component.html',
+})
+export class AssignedContributionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
