3bb1da412c7a50e2488bcb55990d68a9be7cb296
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-06-03 01:28:19 +0300

    Added separate styles file for problem-description component

--- src/app/problems/components/problem-description/problem-description.component.ts
+++ src/app/problems/components/problem-description/problem-description.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { LanguageService } from 'src/app/base/language.base';
 import { ProblemReponse } from 'src/app/models/problem.model';
 
 @Component({
   selector: 'app-problem-description',
   templateUrl: './problem-description.component.html',
+  styleUrls: ['problem-description.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProblemDescriptionComponent {
   @Input() problem!: ProblemReponse;
